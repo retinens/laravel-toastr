3ef637742f22136d96e642e470f1ea3ceb8060cf
WhereIsLucas <lucas@retinens.com>
2022-06-14 08:48:45 +0200

    first commit

--- src/functions.php
+++ src/functions.php
@@ -3,7 +3,7 @@
 if (! function_exists('toastr')) {
     function toastr($message = null, $title = null, $level = 'info')
     {
-        $toaster = app('toaster');
+        $toaster = app('toastr');
         if (! is_null($message)) {
             return $toaster->toast($message, $level, $title);
         }
