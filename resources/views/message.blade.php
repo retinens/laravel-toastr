3711ad495730249fdaeee92ab5f6c7f73eca599a
Lucas Houssa <lucas@retinens.com>
2023-01-15 15:26:44 +0100

    wiating for document ready

--- resources/views/message.blade.php
+++ resources/views/message.blade.php
@@ -1,8 +1,10 @@
 @if (session('toasts', collect())->count())
     <script>
-        @foreach (session('toasts', collect()) as $item)
-        toastr.{{$item["level"]}}('{{ $item["message"] }}','{{ $item["title"] }}')
-        @endforeach
+        document.addEventListener('DOMContentLoaded', function () {
+            @foreach (session('toasts', collect()) as $item)
+            toastr.{{$item["level"]}}('{{ $item["message"] }}','{{ $item["title"] }}')
+            @endforeach
+        }, false);
     </script>
     @php
         session()->forget('toasts')
